b67bc1b082f82ce0bd65200ef733089378fa3d12
jessecogollo <cogollo87@gmail.com>
2014-05-14 14:07:57 -0500

    Configuracion inicial sw

--- PracticaFinal/PracticaFinal.ino
+++ PracticaFinal/PracticaFinal.ino
@@ -1,9 +1,12 @@
-#define 
-#define 
-#define
+#define sw1 PINC, 0
+#define sw2 PINC, 1
+#define sw3 PINC, 2
+#define sw4 PINC, 3
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(48, 49, 45, 44, 43, 42);
 
 void setup() {
-
+  
 }
 
 void loop() {
