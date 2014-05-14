5a6fdad9fb523341902abfb55953481b11fcf0de
jessecogollo <cogollo87@gmail.com>
2014-05-14 14:36:39 -0500

    Inicio de configuraciion sw

--- PracticaFinal/PracticaFinal.ino
+++ PracticaFinal/PracticaFinal.ino
@@ -6,9 +6,13 @@
 LiquidCrystal lcd(48, 49, 45, 44, 43, 42);
 
 void setup() {
-  
+  pinMode(47, OUPUT);
+  digitalWrite(47,0);
+  lcd.begin(16,2);
 }
 
 void loop() {
-
+  if(bitRead(PINC, sw1) == HIGH){
+    
+  }
 }
