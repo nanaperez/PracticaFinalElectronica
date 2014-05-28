fc301cc868feb974e5ea549af690c51564569137
jessecogollo <cogollo87@gmail.com>
2014-05-28 13:31:36 -0500

    cambios de hace ocho dias.

--- PracticaFinal/PracticaFinal.ino
+++ PracticaFinal/PracticaFinal.ino
@@ -1,3 +1,4 @@
+
 #define sw1 PINC, 0
 #define sw2 PINC, 1
 #define sw3 PINC, 2
@@ -6,13 +7,19 @@
 LiquidCrystal lcd(48, 49, 45, 44, 43, 42);
 
 void setup() {
-  pinMode(47, OUPUT);
+  pinMode(47, OUTPUT);
   digitalWrite(47,0);
   lcd.begin(16,2);
 }
 
 void loop() {
-  if(bitRead(PINC, sw1) == HIGH){
-    
-  }
+  //if((bitRead(PINC, sw1) == HIGH) && (bitRead(PINC, sw2) == HIGH) && (bitRead(PINC, sw3) == HIGH) && (bitRead(PINC, sw4) == HIGH)){
+    lcd.setCursor(1,0);
+    lcd.print("Bienvenido");
+    lcd.setCursor(1,1);
+    lcd.print("Gracias!!!");
+  //}
+  //else if((bitRead(PINC, sw1) == LOW) && (bitRead(PINC, sw2) == LOW) && (bitRead(PINC, sw3) == LOW) && (bitRead(PINC, sw4) == LOW)){
+    //lcd.clear();
+  //}
 }
