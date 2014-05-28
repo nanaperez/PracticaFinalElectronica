0994829782daa3af8fd1dd3a5fbf99f73aa14d9e
Alejandra Perez <nanaperez01@gmail.com>
2014-05-28 15:11:12 -0500

    Subiendo 28/05

--- PracticaFinal/PracticaFinal.ino
+++ PracticaFinal/PracticaFinal.ino
@@ -3,14 +3,19 @@
 #define sw3 PINC, 2
 #define sw4 PINC, 3
 
-#include <LiquidCrystal.h>    //agrega la libreria del LCD
+#include <LiquidCrystal.h>//agrega la libreria del LCD
+#include <stock.h>
 
 LiquidCrystal lcd(48,49,45,44,43,42);    //inicializa la libreria y define los pines digitales para el LCD
 
+char* almacen[10][2];
+char* secciones[5];
+  
 void setup() {
   pinMode(47,OUTPUT);
   digitalWrite(47,0);
   lcd.begin(16,2);
+  stock();
 }
 
 void loop() {
@@ -18,30 +23,30 @@ void loop() {
     lcd.setCursor(1,0);
     lcd.print("Bienvenido!!");
     lcd.setCursor(1,1);
-     lcd.print("Gracias");
+    lcd.print("Gracias");
   }
-  else if (bitRead(PINC, sw2)==LOW && bitRead(PINC, sw3) == LOW && bitRead(PINC, sw4) == LOW){
+  else if (bitRead(PINC, sw2)==LOW && bitRead(PINC, sw3)==LOW && bitRead(PINC, sw4)==LOW){
     lcd.clear();
   }
   
   if (bitRead(PINC, sw2)==HIGH){
       lcd.setCursor(1,0);
       lcd.print("SECCION #");
-      //lcd.setCursor(1,1);
-      //lcd.print("JESSECOGOLLO");
+      lcd.setCursor(1,1);
+      lcd.print(secciones[0]);
   }
 
-  else if (bitRead(PINC, sw1)==LOW && bitRead(PINC, sw3) == LOW && bitRead(PINC, sw4) == LOW){
+  else if (bitRead(PINC, sw1)==LOW && bitRead(PINC, sw3)==LOW && bitRead(PINC, sw4)==LOW){
     lcd.clear();
   }
   
   if(bitRead(PINC, sw3)==HIGH){
      lcd.setCursor(1,0);
-     lcd.print("CANTIDAD EN STOCK");
+     lcd.print("CANTIDAD 10");
      lcd.setCursor(1,1);
-     lcd.print("#");
+     lcd.print("EN STOCK");
   }
-  else if (bitRead(PINC, sw1)==LOW && bitRead(PINC, sw2) == LOW && bitRead(PINC, sw4) == LOW){
+  else if (bitRead(PINC, sw1)==LOW && bitRead(PINC, sw2)==LOW && bitRead(PINC, sw4)==LOW){
     lcd.clear();
   }
   
@@ -51,7 +56,37 @@ void loop() {
      lcd.setCursor(1,1);
      lcd.print("SI    NO");
   }
-  else if (bitRead(PINC, sw1)==LOW && bitRead(PINC, sw2) == LOW && bitRead(PINC, sw3) == LOW){
+  else if (bitRead(PINC, sw1)==LOW && bitRead(PINC, sw2)==LOW && bitRead(PINC, sw3)==LOW){
     lcd.clear();
   }
+}
+
+void stock() {
+  
+  secciones[0] = "s1 - ";
+  secciones[1] = "s2";
+  secciones[2] = "s3";
+  secciones[3] = "s4";
+  secciones[4] = "s5";
+  
+  almacen[0][0] = "papas";
+  almacen[0][1] = "s1";
+  almacen[1][0] = "huevos";
+  almacen[1][1] = "s1";
+  almacen[2][0] = "arroz";
+  almacen[2][1] = "s2";
+  almacen[3][0] = "maiz";
+  almacen[3][1] = "s2";
+  almacen[4][0] = "carne";
+  almacen[4][1] = "s3";
+  almacen[5][0] = "pollo";
+  almacen[5][1] = "s3";
+  almacen[6][0] = "pescado";
+  almacen[6][1] = "s4";
+  almacen[7][0] = "arepa";
+  almacen[7][1] = "s4";
+  almacen[8][0] = "frutas";
+  almacen[8][1] = "s5";
+  almacen[9][0] = "verduras";
+  almacen[9][1] = "s5";  
 }
---
+++ PracticaFinal/stock.h
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+
+char* seccion [];
+char* producto[];
+
+class stock {
+public:
+	void seccion();
+};
