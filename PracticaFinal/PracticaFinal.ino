1da0d649a984761edc314797d988346b2c93afea
Alejandra Perez <nanaperez01@gmail.com>
2014-05-28 21:38:41 -0500

    Corrigiendo cositas

--- PracticaFinal/PracticaFinal.ino
+++ PracticaFinal/PracticaFinal.ino
@@ -9,7 +9,7 @@
 LiquidCrystal lcd(48,49,45,44,43,42);    //inicializa la libreria y define los pines digitales para el LCD
 
 char* almacen[10][3];
-char* producto[10];
+char* producto[8];
 char* secciones[4];
   
 void setup() {
@@ -31,7 +31,7 @@ void loop() {
   }
   
   if (bitRead(PINC, sw2)==HIGH){
-    for(int i = 0; i < 9; i++){  
+    for(int i = 0; i < 8; i++){  
       for(int j = 0; j < 2; j++){
         lcd.setCursor(1,0);
         lcd.print(almacen[i][0]);
@@ -48,7 +48,7 @@ void loop() {
   }
   
   if(bitRead(PINC, sw3)==HIGH){
-     for(int i = 0; i < 9; i++){  
+     for(int i = 0; i < 8; i++){  
       for(int j = 0; j < 2; j++){
         lcd.setCursor(1,0);
         lcd.print(almacen[i][1]);
@@ -79,7 +79,7 @@ void stock() {
   secciones[0] = "s1 - lacteos    ";
   secciones[1] = "s2 - congelados ";
   secciones[2] = "s3 - granos     ";
-  secciones[3] = "s4 - legumbreria";
+  secciones[3] = "s4 - aseo       ";
   
   producto[0] = "leche   ";
   producto[1] = "huevos  ";
@@ -87,33 +87,33 @@ void stock() {
   producto[3] = "pollo   ";
   producto[4] = "arroz   ";
   producto[5] = "maiz    ";
-  producto[6] = "trapo   ";
-  producto[7] = "escoba  ";
+  producto[6] = "escoba  ";
+  producto[7] = "jabon   ";
   producto[8] = "frutas  ";
   producto[9] = "verduras";
   
-  almacen[0][0] = "s1";
+  almacen[0][0] = "s1 - lacteos    ";
   almacen[0][1] = "leche ";
   almacen[0][2] = "10    ";
-  almacen[1][0] = "s1";
+  almacen[1][0] = "s1 - lacteos    ";
   almacen[1][1] = "huevos";
   almacen[1][2] = "15    ";
-  almacen[2][0] = "s2";
+  almacen[2][0] = "s2 - congelados ";
   almacen[2][1] = "carne ";
   almacen[2][2] = "19    ";
-  almacen[3][0] = "s2";
+  almacen[3][0] = "s2 - congelados ";
   almacen[3][1] = "pollo ";
-  almacen[3][2] = "1    ";
-  almacen[4][0] = "s3";
+  almacen[3][2] = "16    ";
+  almacen[4][0] = "s3 - granos     ";
   almacen[4][1] = "arroz ";
-  almacen[4][2] = "99    ";
-  almacen[5][0] = "s3";
+  almacen[4][2] = "45    ";
+  almacen[5][0] = "s3 - granos     ";
   almacen[5][1] = "maiz  ";
   almacen[5][2] = "34    ";
-  almacen[6][0] = "s4";
-  almacen[6][1] = "trapo ";
+  almacen[6][0] = "s4 - aseo       ";
+  almacen[6][1] = "escoba ";
   almacen[6][2] = "43    ";
-  almacen[7][0] = "s4";
+  almacen[7][0] = "s4 - aseo       ";
   almacen[7][1] = "jabon ";
   almacen[7][2] = "29    ";
 }
